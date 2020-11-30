c108a7ff55c1f933238957f5c8d5c64f8158ac22
jpshields19 <jpshields19@gmail.com>
2020-11-30 08:20:05 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,3 @@
-output=$(printf \
-    'LOCAL_REPOSITORY=${settings.localRepository}\n'\
-    'GROUP_ID=${project.groupId}\n'
-    'ARTIFACT_ID=${project.artifactId}\n'\
-    'POM_VERSION=${project.version}\n0\n' \
-  | mvn help:evaluate --non-recursive )
-
-localRepository=$(echo "$output" | grep '^LOCAL_REPOSITORY' | cut -d = -f 2)
-groupId=$(echo "$output" | grep '^GROUP_ID' | cut -d = -f 2)
-artifactId=$(echo "$output" | grep '^ARTIFACT_ID' | cut -d = -f 2)
-pomVersion=$(echo "$output" | grep '^POM_VERSION' | cut -d = -f 2)
+GROUP_ID=`echo -e 'setns x=http://maven.apache.org/POM/4.0.0\ncat /x:project/x:groupId/text()' | xmllint --shell pom.xml | grep -v /`
+ARTIFACT_ID=`echo -e 'setns x=http://maven.apache.org/POM/4.0.0\ncat /x:project/x:artifactId/text()' | xmllint --shell pom.xml | grep -v /`
+VERSION=`echo -e 'setns x=http://maven.apache.org/POM/4.0.0\ncat /x:project/x:version/text()' | xmllint --shell pom.xml | grep -v /`
