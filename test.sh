d0c400c7484a07359521cdad475cdf65924bebca
jpshields19 <jpshields19@gmail.com>
2020-12-02 07:55:31 -0600

    Delete test.sh

--- test.sh
+++
@@ -1,3 +0,0 @@
-GROUP_ID=`echo -e 'setns x=http://maven.apache.org/POM/4.0.0\ncat /x:project/x:groupId/text()' | xmllint --shell pom.xml | grep -v /`
-ARTIFACT_ID=`echo -e 'setns x=http://maven.apache.org/POM/4.0.0\ncat /x:project/x:artifactId/text()' | xmllint --shell pom.xml | grep -v /`
-VERSION=`echo -e 'setns x=http://maven.apache.org/POM/4.0.0\ncat /x:project/x:version/text()' | xmllint --shell pom.xml | grep -v /`
